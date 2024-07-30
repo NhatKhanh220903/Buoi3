390b594d9386b0117d08d59fa29e861bc45db2f8
Nhat Khanh <phamvonhatkhanh@gmail.com>
2024-07-31 02:06:02 +0700

    hoan thanh cau2

--- Cau2.cpp
+++ Cau2.cpp
@@ -13,7 +13,7 @@ void nhapSoNguyenDuong(int& x)
 			printf("Nhap lai so nguyen duong!\n");
 	} while (x <= 0);
 }
-void taoMaTranVuong(int a[][MAX],int& n)
+void taoMaTranVuong(int a[][MAX], int& n)
 {
 	do {
 		printf("Hay cho biet bac ma tran vuong va lon hon 5: ");
@@ -31,7 +31,7 @@ void taoMaTranVuong(int a[][MAX],int& n)
 	}
 }
 
-void xuatMaTranVuong(int a[][MAX],int n)
+void xuatMaTranVuong(int a[][MAX], int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -49,7 +49,7 @@ void xuatDCC(int a[][MAX], int n)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			if(i==j)
+			if (i == j)
 				printf("%5d", a[i][j]);
 		}
 	}
@@ -78,11 +78,83 @@ void cau3(int a[][MAX], int n)
 		inDCSS(a, n, -k);
 	}
 }
+void cau4(int a[][MAX], int n)
+{
+	int max = INT_MIN;
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) {
+			if (a[i][j] > max) {
+				max = a[i][j];
+			}
+		}
+	}
+	printf("Phan tu lon nhat tam giac tren DCC la: %d\n", max);
+}
+void swap(int& a, int& b)
+{
+	int x = a;
+	a = b;
+	b = x;
+}
+void cau5(int a[][MAX], int n, int b[])
+{
+	int k = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			b[k++] = a[i][j];
+		}
+	}
+	for (int i = 0; i < k - 1; i++) {
+		for (int j = 0; j < k - 1 - i; j++) {
+			if (b[j] > b[j + 1]) {
+				swap(b[j], b[j + 1]);
+			}
+		}
+	}
+	for (int i = 0; i < k; i++)
+	{
+		printf("%d ", b[i]);
+	}
+	k = 0;
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 0) {
+			for (int j = 0; j < n; j++) {
+				a[i][j] = b[k++];
+			}
+		}
+		else {
+			for (int j = n - 1; j >= 0; j--) {
+				a[i][j] = b[k++];
+			}
+		}
+	}
+
+	printf("Ma tran sau khi xep la: \n");
+	xuatMaTranVuong(a, n);
+}
+void cau6(int a[][MAX], int n, int b[])
+{
+	for (int i = 0; i < n; i++) {
+			b[i] = a[i][i];
+	}
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - 1 - i; j++) {
+			if (b[j] > b[j + 1]) {
+				swap(b[j], b[j + 1]);
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		a[i][i] = b[i];
+	}
+	printf("Ma tran sau khi xep la: \n");
+	xuatMaTranVuong(a, n);
+}
 int main()
 {
-	int a[MAX][MAX], n;
+	int a[MAX][MAX], n,b[MAX*MAX];
 	taoMaTranVuong(a, n);
 	xuatMaTranVuong(a, n);
 	//xuatDCC(a, n);
-	
+	cau6(a,n,b);
 }
